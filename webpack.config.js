526be89aaf063ab019029307de54c0dd0d6b7113
Diego Muracciole <diegomuracciole@gmail.com>
2017-09-28 21:37:07 -0300

    1.6 Setting up a development server

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,14 @@
 const path = require('path');
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  context: path.join(__dirname, 'src'),
   entry: "./index.js",
   output: {
    path: path.resolve(__dirname, "dist"),
    filename: "bundle.js",
+   publicPath: '/',
   },
   module: {
     rules: [
@@ -14,5 +17,11 @@ module.exports = {
         loader: "babel-loader",
       }
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+      inject: 'body',
+    }),
+  ]
 };
