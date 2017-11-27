e0778dfa47c405fb949514bd85d6c848dd805e80
Diego Muracciole <diegomuracciole@gmail.com>
2017-11-27 19:11:25 -0300

    4.4 Add links to different routes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { NavLink } from 'react-router-dom';
+
+const HeaderLink = ({ children, ...props }) => (
+  <NavLink
+    exact
+    className="p1 mx2 black rounded text-decoration-none"
+    activeClassName="bg-white"
+    {...props}
+  >
+    {children}
+  </NavLink>
+);
 
 const Header = () => (
-  <header className="px4">
+  <header className="flex justify-between items-center px4">
     <h1 className="h1">🍽 MyRecipes</h1>
+    <nav>
+      <HeaderLink to="/">Home</HeaderLink>
+      <HeaderLink to="/favorites">Favorites</HeaderLink>
+    </nav>
   </header>
 );
+
+HeaderLink.propTypes = {
+  children: PropTypes.node,
+};
 
 export default Header;
