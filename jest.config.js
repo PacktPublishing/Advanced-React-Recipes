02fea3dfb8acf2a764ff2830327d9968f995a2cc
Diego Muracciole <diegomuracciole@gmail.com>
2018-02-14 19:28:20 -0300

    5.3 Mock static assets in our tests

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
   modulePathIgnorePatterns: ['/dist/', '/node_modules/'],
   testRegex: '/src/.*?(Spec)\\.js$',
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|svg)$': '<rootDir>/src/utils/fileMock.js',
+  },
 };
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
+import PacktLogo from '../static/images/logo.png';
 
 const HeaderLink = ({ children, ...props }) => (
   <NavLink
@@ -15,7 +16,11 @@ const HeaderLink = ({ children, ...props }) => (
 
 const Header = () => (
   <header className="flex justify-between items-center px4">
-    <h1 className="h1">🍽 MyRecipes</h1>
+    <div>
+      <h1 className="h1 inline-block mr2">🍽 MyRecipes</h1>
+      <span className="mr1">by</span>
+      <img src={PacktLogo} alt="by Packt" style={{ width: 20 }} />
+    </div>
     <nav>
       <HeaderLink to="/">Home</HeaderLink>
       <HeaderLink to="/favorites">Favorites</HeaderLink>
