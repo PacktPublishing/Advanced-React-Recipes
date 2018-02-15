032a1cb70ecb9486a4c337024a9edafcff7b3370
Diego Muracciole <diegomuracciole@gmail.com>
2018-02-14 22:19:37 -0300

    6.2: Add handy redux middlewares

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,19 @@
-import { createStore } from 'redux';
+/* eslint-disable global-require */
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import persistState from 'redux-localstorage';
 import { makeRootReducer } from '../reducers';
 
+const middleware = [thunk];
+
+if (process.env.NODE_ENV !== 'production') {
+  const { createLogger } = require('redux-logger');
+  middleware.push(createLogger({ collapsed: true, diff: true }));
+}
+
+const enhancer = compose(applyMiddleware(...middleware), persistState());
+
 // Store Instantiation and HMR Setup
-const store = createStore(makeRootReducer());
+const store = createStore(makeRootReducer(), enhancer);
 
 export default store;
