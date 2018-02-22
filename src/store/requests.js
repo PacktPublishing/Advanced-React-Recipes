0d26811551537da19c84c4a96b0c3d14657b1f4c
Diego Muracciole <diegomuracciole@gmail.com>
2018-02-22 01:44:12 -0300

    7.4: Add resource time-to-live feature

--- src/store/requests.js
+++ src/store/requests.js
@@ -1,5 +1,29 @@
+const cache = new Map();
+
+// Store in cache with ttl for 5 minutes
+const set = (key, value) => cache.set(key, { value, ttl: Date.now() + 300000 });
+const find = key => cache.get(key);
+
 const requests = () => next => action => {
   const { type, remote, method, body, ...others } = action;
+
+  if (!method || method === 'GET') {
+    const cached = find(remote);
+
+    // If endpoint is in cache and still valid,
+    // return success action with cache
+    if (cached && cached.ttl > Date.now()) {
+      next({
+        type: `${type}_SUCCESS`,
+        data: cached.value.data,
+        cached: true,
+        ...others,
+      });
+
+      // Return a promise, to be coherent with `request` answer.
+      return new Promise(resolve => resolve(cached.value));
+    }
+  }
 
   if (remote) {
     return fetch(`${API_URL}${remote}`, {
@@ -8,6 +32,12 @@ const requests = () => next => action => {
     })
       .then(res => res.json())
       .then(data => next({ type: `${type}_SUCCESS`, data, ...others }))
+      .then(data => {
+        if (!method || method === 'GET') {
+          set(remote, data);
+        }
+        return data;
+      })
       .catch(error => {
         next({ type: `${type}_FAILURE`, error });
         throw new Error(error);
