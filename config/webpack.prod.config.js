509e1c6b1e536bb6cc4ea468f9a53cab485b60d7
Diego Muracciole <diegomuracciole@gmail.com>
2018-03-21 20:35:06 -0300

    10.4: Deploying to Surge

--- config/webpack.prod.config.js
+++ config/webpack.prod.config.js
@@ -1,12 +1,21 @@
 /* eslint-disable */
 const webpack = require('webpack');
 const config = require('./webpack.config.js');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 require('dotenv').config();
 
 config.plugins.push(
   new webpack.DefinePlugin({
     API_URL: JSON.stringify(process.env.PROD_API_URL),
+  })
+);
+
+config.plugins.push(
+  new HtmlWebpackPlugin({
+    filename: '200.html',
+    template: 'index.html',
+    inject: 'body',
   })
 );
 
